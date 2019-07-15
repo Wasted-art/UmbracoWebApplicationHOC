a9e76ea2458a55e209102245382fb318ab5508ba
Wasted-Art <wastedartz@gmail.com>
2019-07-15 08:43:51 +0200

    Initial commit

---
+++ Controllers/RegisterController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Models;
+using Umbraco.Web.Mvc;
+
+namespace UmbracoWebApplicationHOC.Controllers
+{
+    public class RegisterController : SurfaceController
+    {
+        public ActionResult Register(RegisterModel model)
+        {
+            if (!ModelState.IsValid)
+                return CurrentUmbracoPage();
+            //Member Creation Code
+            var memberService = Services.MemberService;
+
+            if (memberService.GetByEmail(model.Email) != null)
+            {
+                ModelState.AddModelError("", "A Member with that email already exists");
+                return CurrentUmbracoPage();
+            }
+
+            var member = memberService.CreateMemberWithIdentity(model.Email, model.Email, model.Name, "Member");
+
+            //member.SetValue("bio", model.Biography);
+            //member.SetValue("avatar", model.Avatar);
+
+            memberService.SavePassword(member, model.Password);
+            Members.Login(model.Email, model.Password);
+
+            //memberService.Save(member);
+
+            return Redirect("/");
+        }
+    }
+}
---
+++ Models/RegisterModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace UmbracoWebApplicationHOC.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        //public string Biography { get; set; }
+
+        //public HttpPostedFileBase Avatar { get; set; }
+    }
+}
